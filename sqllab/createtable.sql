2da4f0f5dde6c5994421b3af88634c935e0a770a
梁轩振 <liangxuanzhen@liangxuanzhendeMacBook-Pro.local>
2024-04-08 23:51:45 -0500

    create table updated

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,13 +1,9 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quaketime time with time zone,
   latitude real,
   longitude real,
   depth real,
   id text,
-  place text,
-  type text,
-  locationSource text,
-  magSource text
+  locationSource text
 
 );
