67049d6aff03c4e326b635383abca9f0e8b0c49a
liangl <liangl@carleton.edu>
2024-04-26 10:37:31 -0500

    test and templates updated

--- flasklab/part2/template-test.py
+++ flasklab/part2/template-test.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 import random
+import psycopg2
 
 app = Flask(__name__)
 
@@ -9,24 +10,43 @@ def welcome():
     return render_template("index.html")
 
 @app.route('/randdeath')
-def rand(low, high):
-    #Input values that come from a URL (i.e., @app.route)
-    #   are always strings so I need to convert the type to int
-    low_int = int(low)
-    high_int = int(high)
-    
-    num = random.randint(low_int, high_int)
-    return render_template("random.html", randNum = num)
+def rand():
+    return render_template("randtrolling.html")
 
 @app.route('/randborn')
-def rand(low, high):
-    #Input values that come from a URL (i.e., @app.route)
-    #   are always strings so I need to convert the type to int
-    low_int = int(low)
-    high_int = int(high)
+def rand():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,   
+        database="liangl",
+        user="liangl",
+        password="ruby383expo")
+
+    cur = conn.cursor()
+
+    sqlfirst = "SELECT Firstname FROM nametime"
+    sqllast = "SELECT Lastname FROM nametime"
+    sqlyear = "SELECT year FROM nametime"
+    sqlcity = "SELECT city FROM uspop1k"
     
-    num = random.randint(low_int, high_int)
-    return render_template("random.html", randNum = num)
+    cur.execute( sqlfirst )
+    firstrowlist = cur.fetchall()
+
+    cur.execute( sqllast )
+    lastrowlist = cur.fetchall()
+
+    cur.execute( sqlyear )
+    yearrowlist = cur.fetchall()
+
+    cur.execute( sqlcity )
+    cityrowlist = cur.fetchall()
+    
+    firstnum = random.randint(0, 9)
+    lastnum = random.randint(0, 9)
+    yearnum = random.randint(0,19)
+    citynum = random.randint(0,50)
+    return render_template("randborn.html", firstname = firstrowlist[firstnum], lastname = lastrowlist[lastnum], year = yearrowlist[yearnum], city = cityrowlist[citynum])
 
 
 if __name__ == '__main__':
