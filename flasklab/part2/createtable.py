5580ba87133c264581b583dc11e4a5ee169f6d71
liangl <liangl@carleton.edu>
2024-04-26 09:59:46 -0500

    createtable added

---
+++ flasklab/part2/createtable.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+# This function creates the two tables
+def create_tables():
+    
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,   
+        database="liangl",
+        user="liangl",
+        password="ruby383expo")
+
+    if conn is not None:
+            cur = conn.cursor()
+
+            sql_nt = """DROP TABLE IF EXISTS name&time;
+                           CREATE TABLE name&time (
+                             Firstname text,
+                             Lastname text,
+                             year real
+                           );"""
+            
+            cur.execute( sql_nt )
+            print("Table created.")
+            
+
+            conn.commit()
+            
+    else:
+        print( "Problem with Connection" )
+    return None
+
+create_tables()
