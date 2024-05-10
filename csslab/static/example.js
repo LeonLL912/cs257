99a97257b53e2a210754915463854c4258aec4bc
liangl <liangl@carleton.edu>
2024-05-09 23:04:27 -0500

    csslab updated

--- csslab/static/example.js
+++ csslab/static/example.js
@@ -1,5 +1,24 @@
+
+the_heading = document.getElementById("hello");
+const color = the_heading.style.color;
+
+the_heading.onclick = function(){ the_heading.innerText="Goodbye World!"};
+
+the_paragraph = document.getElementById("paragraph1");
+
+var counter = 1;
 function changeColor() {
-  the_heading = document.getElementById("hello");
-  the_heading.style.color = "red";
-  console.log("I just changed the color to: " + the_heading.style.color)  
+  if (counter % 2 == 1){
+  	text_input_element = document.getElementById("user-color");
+  	new_color = text_input_element.value;
+		the_heading.style.color = new_color;
+  	button.innerText="OMG, this is even worse! Change The Color Back";
+    counter++;
+  }
+  else{
+		the_heading.style.color = color;
+  	button.innerText="Change Color"; 
+    counter++;
+  }
+  
 }
